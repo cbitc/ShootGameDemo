477c3570db2a8733436ed5dbf6e45355a46e2375
cbitc <2413809064@qq.com>
2023-12-13 18:28:23 +0800

    first

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/86173/Desktop/ccGamebin/bin/libmain.dll.a"
+  "C:/Users/86173/Desktop/ccGamebin/bin/main.exe"
+  "C:/Users/86173/Desktop/ccGamebin/bin/main.exe.manifest"
+  "C:/Users/86173/Desktop/ccGamebin/bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
